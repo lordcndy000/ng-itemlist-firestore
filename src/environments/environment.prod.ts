383686e74505621e80108fadadb9d5cf813adf12
lordcndy000 <lordwill.mabalot@gmail.com>
2018-01-25 20:49:03 +0800

    [UPDATE] Build for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAAhuS6Q3LPLogq8e-oE_8FBEALWreGu5k',
+    authDomain: 'ng-firestore-31738.firebaseapp.com',
+    databaseURL: 'https://ng-firestore-31738.firebaseio.com',
+    projectId: 'ng-firestore-31738',
+    storageBucket: 'ng-firestore-31738.appspot.com',
+    messagingSenderId: '316366975242'
+  }
 };
